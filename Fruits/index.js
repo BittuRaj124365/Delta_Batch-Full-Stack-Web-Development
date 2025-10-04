0f1ef7a220b6852c40e696c97fbadea795c357d2
Bittu Raj <bitturaj124365@gmail.com>
2025-10-04 11:40:07 +0530

    Add files via upload

---
+++ Fruits/index.js
@@ -0,0 +1,7 @@
+const apple=require("./apple");
+const banana=require("./banana");
+const litchi=require("./litchi");
+const carrot=require("./carrot");
+
+let fruits=[apple,banana,litchi,carrot];
+module.exports=fruits;
